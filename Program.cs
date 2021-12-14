c84d7ff4fb82fe4402da291cb384db02977857e9
max7 <max7@DESKTOP-DQ2AN4C>
2021-12-14 10:06:02 +0200

    1 commit push to git

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+
+    class Max
+    {
+        string name;
+        int age;
+
+        public Max() { name = "Empty";age = 18; }
+        public Max(string n) { name = n;age = 18; }
+        public Max(string n, int a) { name = n;age = a; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name:{name} Age:{age}");
+        
+        }
+    
+    
+    
+    
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "Ivan";
+            int b = 56;
+
+            Max mix = new Max("mix", 18);
+            Console.WriteLine("Hello World!");
+
+            Max ivan = new Max(a, b);
+
+            Max boo = new Max("Boo");
+            boo.Print();
+
+            Max poo = new Max();
+            poo.Print();
+
+
+        }
+    }
+}
